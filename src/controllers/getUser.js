b1f4abb1aab1518a0f9922095b987f0b4e3d501d
victordurco <victordurco@gmail.com>
2021-11-19 15:42:05 -0300

    get user controller and tests done

---
+++ src/controllers/getUser.js
@@ -0,0 +1,39 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable consistent-return */
+
+import connection from '../database.js';
+
+const getUser = async (req, res) => {
+  try {
+    const token = req.headers.authorization?.replace('Bearer ', '');
+    if (!token) {
+      res.sendStatus(401);
+      return;
+    }
+
+    const userQuery = await connection.query(
+      `
+        SELECT
+          users.*
+        FROM users
+          JOIN sessions
+            ON users.id = sessions.user_id
+        WHERE sessions.token = $1`,
+      [token]
+    );
+
+    const user = userQuery.rows[0];
+
+    if (!user) {
+      return res.sendStatus(401);
+    }
+
+    return res.status(200).send({
+      name: user.name,
+    });
+  } catch (err) {
+    return res.sendStatus(500);
+  }
+};
+
+export default getUser;
